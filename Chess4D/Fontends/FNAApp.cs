330b0e440d1720c1565de2b88643922b8ab57bd4
Ajay Kristipati <ajayk111@users.noreply.github.com>
2019-08-14 09:20:15 -0400

    more FNA work

--- Chess4D/Fontends/FNAApp.cs
+++ Chess4D/Fontends/FNAApp.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Collections.Generic;
-using Microsoft.Xna;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -8,31 +9,40 @@ namespace Chess4D
 {
     public class FNAApp : IFrontend
     {
+        public delegate bool StartSelectingMove();
+
+        //event 
+        
+        public delegate bool finishedSelectingMove();
+
+        public delegate bool startedSelectingPiece();
+
+        public delegate bool finishedSelectingPiece();
         public FNAApp()
         {
-            using(Game1 game = new Game1())
+            using (var game = new Game1())
             {
-                 game.Run();
+                Task.Factory.StartNew(() => game.Run());
             }
         }
 
         public void PrintBoard()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public sbyte? SelectMove(IEnumerable<sbyte> moves)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public sbyte SelectPiece(Teams currentPlayer)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game1 : Game
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
@@ -42,42 +52,43 @@ namespace Chess4D
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-        
+
         //Load Non content stuff
         protected override void Initialize()
         {
             base.Initialize();
         }
-        
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
- 
+
             // TODO: use this.Content to load your game content here
         }
- 
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
-        
+
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
- 
+                Exit();
+
             // TODO: Add your update logic here
- 
+
             base.Update(gameTime);
         }
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
- 
+
             // TODO: Add your drawing code here
- 
+
             base.Draw(gameTime);
         }
     }
